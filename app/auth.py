acf2583411055cd33408facaf10d46e623092c9f
WiktorJ02 <wjurczak02@gmail.com>
2024-05-27 07:33:04 +0200

    working on register form

--- app/auth.py
+++ app/auth.py
@@ -9,12 +9,19 @@ from app import authentication as at
 
 
 class RegistrationForm(FlaskForm):
+    login = StringField('Login: ')
     first_name = StringField('Name:')
     last_name = StringField("Last name")
     birth_date = DateField('Birth date', format='%Y-%m-%d', validators=[validators.DataRequired()])
+    submit = SubmitField('Register')
     
 
-@at.route('/register')
+@at.route('/register', methods=['GET', 'POST'])
 def register_user():
     form = RegistrationForm()
-    return render_template('auth/registration.html', form = form)
+    if form.validate_on_submit():
+        # Process form data (e.g., save to database)
+        return 'Form successfully submitted'
+    else:
+        print(form.errors)  # Print form errors to console for debugging
+    return render_template('auth/registration.html', form=form)
