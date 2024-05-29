dae8d8d7111411c28007c7dbc8f3d1ae15842a78
WiktorJ02 <wjurczak02@gmail.com>
2024-05-29 22:54:55 +0200

    database model updated and frontend changes for registration

--- app/models/publications.py
+++ app/models/publications.py
@@ -10,17 +10,19 @@ class Publications(db.Model):
     name = db.Column(db.String(30), nullable = False)
     description = db.Column(db.String(300), nullable = False)
     price = db.Column(db.Float, nullable = False)
+    localization = db.Column(db.String(30), nullable = False)
     creating_user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
     accepting_user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable = True)
     creation_date = db.Column(db.DateTime, default=lambda: datetime.now(pytz.timezone('Europe/Warsaw')))
     image = db.Column(db.String(100))
     is_visible = db.Column(db.Boolean, default=True)
     
-    def __init__(self, name, description, price, creating_user_id, image):\
+    def __init__(self, name, description, price, localization, creating_user_id, image):\
         
         self.name = name
         self.description = description
         self.price = price
         self.creating_user_id = creating_user_id
+        self.localization = localization
         self.image = image
         
