1137d341e622bf8da02a07e85f5302e495255dad
WiktorJ02 <wjurczak02@gmail.com>
2024-06-03 08:30:59 +0200

    register

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.fields import DateField
