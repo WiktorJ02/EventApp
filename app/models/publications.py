611e75400c0c3f610b5a9783d14108ce1d5b4f0c
WiktorJ02 <wjurczak02@gmail.com>
2024-06-05 19:23:33 +0200

    ratings

--- app/models/publications.py
+++ app/models/publications.py
@@ -3,7 +3,7 @@ import pytz
 from datetime import datetime
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy import func
-from ratings import Ratings 
+# from ratings import Ratings 
 
 # Model of Publications table    
 class Publications(db.Model):
@@ -30,15 +30,15 @@ class Publications(db.Model):
         self.image = image
         
         
-    @hybrid_property
-    def average_rating(self):
-        if self.publication_ratings:
-            return sum(rating.rating for rating in self.publication_ratings) / len(self.publication_ratings)
-        return 0
+    # @hybrid_property
+    # def average_rating(self):
+    #     if self.publication_ratings:
+    #         return sum(rating.rating for rating in self.publication_ratings) / len(self.publication_ratings)
+    #     return 0
 
-    @average_rating.expression
-    def average_rating(cls):
-        return (db.session.query(func.avg(Ratings.rating))
-                .filter(Ratings.publication_id == cls.id)
-                .correlate(cls)
-                .as_scalar())
+    # @average_rating.expression
+    # def average_rating(cls):
+    #     return (db.session.query(func.avg(Ratings.rating))
+    #             .filter(Ratings.publication_id == cls.id)
+    #             .correlate(cls)
+    #             .as_scalar())
