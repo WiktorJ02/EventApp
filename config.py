077ac76ca9478f24a976829bfad6611dadf7ecfc
GimmeSomeCoffee <80617954+ogrod0707@users.noreply.github.com>
2024-06-29 15:14:07 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = 'admin'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/event_db'
+    SECRET_KEY = '1234'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/event_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
